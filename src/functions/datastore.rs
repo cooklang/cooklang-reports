8ca331b72f20dfac9987a076433bcf96ed8818ca
Dan Fego <dan.fego@gmail.com>
2025-03-23 22:12:08 -0400

    Further tightened up datastore

--- src/functions/datastore.rs
+++ src/functions/datastore.rs
@@ -3,6 +3,10 @@ use minijinja::{Error as MiniError, ErrorKind, State, Value as MiniValue};
 use serde::Deserialize;
 use yaml_datastore::Datastore;
 
+fn non_key_error(message: &str) -> MiniError {
+    MiniError::new(ErrorKind::NonKey, message.to_owned())
+}
+
 pub fn get_from_datastore(state: &State, key_path: &str) -> Result<MiniValue, MiniError> {
     let key_path = KeyPath::try_from(key_path).unwrap();
 
@@ -10,21 +14,10 @@ pub fn get_from_datastore(state: &State, key_path: &str) -> Result<MiniValue, Mi
     // This is kinda terse, but the expanded version isn't really any better.
     let datastore = state
         .lookup("datastore")
-        .ok_or(MiniError::new(ErrorKind::NonKey, "bad datastore"))
-        .and_then(|x| {
-            Option::<Datastore>::deserialize(x)?
-                .ok_or(MiniError::new(ErrorKind::NonKey, "no datastore"))
-        })?;
+        .ok_or(non_key_error("bad datastore"))
+        .and_then(|x| Option::<Datastore>::deserialize(x)?.ok_or(non_key_error("no datastore")))?;
 
     Ok(datastore
-        .get_with_key_vec(
-            &*key_path.path(),
-            key_path
-                .key_vec()
-                .iter()
-                .map(|s| s.as_str())
-                .collect::<Vec<&str>>()
-                .as_slice(),
-        )
+        .get_with_key_vec(&*key_path.path(), &key_path.key_vec())
         .unwrap())
 }
