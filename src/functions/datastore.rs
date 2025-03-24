fed6d818840f2948e27ba5e181160be9f69cfb97
Dan Fego <dan.fego@gmail.com>
2025-03-23 21:45:49 -0400

    Made datastore lookup a bit more concise

--- src/functions/datastore.rs
+++ src/functions/datastore.rs
@@ -1,35 +1,30 @@
-use minijinja::{Error as MiniError, State, Value as MiniValue};
+use crate::key_path::KeyPath;
+use minijinja::{Error as MiniError, ErrorKind, State, Value as MiniValue};
 use serde::Deserialize;
 use yaml_datastore::Datastore;
 
-pub fn get_from_datastore(state: &State, args: &[MiniValue]) -> Result<MiniValue, MiniError> {
-    if args.len() != 1 {
-        return Err(MiniError::new(
-            minijinja::ErrorKind::InvalidOperation,
-            "db requires exactly 1 argument: key-path",
-        ));
-    }
+pub fn get_from_datastore(state: &State, key_path: &str) -> Result<MiniValue, MiniError> {
+    let key_path = KeyPath::try_from(key_path).unwrap();
 
-    // Validate that the argument is a string, but we don't need to store it
-    args[0].as_str().ok_or_else(|| {
-        MiniError::new(
-            minijinja::ErrorKind::InvalidOperation,
-            "argument must be a string (key-path)",
+    // Lookup datastore. If it exists, convert it from Value to Datastore.
+    // This is kinda terse, but the expanded version isn't really any better.
+    let datastore = state
+        .lookup("datastore")
+        .ok_or(MiniError::new(ErrorKind::NonKey, "bad datastore"))
+        .and_then(|x| {
+            Option::<Datastore>::deserialize(x)?
+                .ok_or(MiniError::new(ErrorKind::NonKey, "no datastore"))
+        })?;
+
+    Ok(datastore
+        .get_with_key_vec(
+            &*key_path.path(),
+            key_path
+                .key_vec()
+                .iter()
+                .map(|s| s.as_str())
+                .collect::<Vec<&str>>()
+                .as_slice(),
         )
-    })?;
-
-    // if let Some(datastore) = state.lookup("datastore") {}
-    // let datastore = state.lookup("datastore").and_then(f)
-    // if let Some(datastore_value) = datastore {
-    //     let datastore = Option::<Datastore>::deserialize(datastore_value).unwrap();
-    // }
-
-    let recipe_template = state.lookup("recipe_template").ok_or_else(|| {
-        MiniError::new(
-            minijinja::ErrorKind::InvalidOperation,
-            "recipe_template not found in context",
-        )
-    })?;
-
-    recipe_template.call_method(state, "db", args)
+        .unwrap())
 }
