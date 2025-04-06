22dbcd2d4b74d431ba85d282d4260b7d29afeaac
Dan Fego <dan.fego@gmail.com>
2025-04-06 00:22:13 -0400

    Added new model for ingredients

---
+++ src/model/ingredient.rs
@@ -0,0 +1,80 @@
+//! Model for ingredient.
+#[derive(Debug, Clone)]
+pub(crate) struct Ingredient(cooklang::Ingredient);
+
+impl From<cooklang::Ingredient> for Ingredient {
+    fn from(ingredient: cooklang::Ingredient) -> Self {
+        Self(ingredient)
+    }
+}
+
+impl minijinja::value::Object for Ingredient {
+    fn repr(self: &std::sync::Arc<Self>) -> minijinja::value::ObjectRepr {
+        minijinja::value::ObjectRepr::Plain
+    }
+
+    fn get_value(self: &std::sync::Arc<Self>, key: &minijinja::Value) -> Option<minijinja::Value> {
+        match key.as_str()? {
+            "note" => self.0.note.as_ref().map(minijinja::Value::from),
+            "name" => Some(minijinja::Value::from(&self.0.name)),
+            "alias" => self.0.alias.as_ref().map(minijinja::Value::from),
+            "quantity" => self
+                .0
+                .quantity
+                .as_ref()
+                .map(ToString::to_string)
+                .map(minijinja::Value::from),
+            _ => None,
+        }
+    }
+
+    fn render(self: &std::sync::Arc<Self>, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+    where
+        Self: Sized + 'static,
+    {
+        match &self.0.quantity {
+            Some(quantity) => write!(f, "{quantity} {}", self.0.display_name()),
+            None => write!(f, "{}", self.0.display_name()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cooklang::{Converter, CooklangParser, Extensions, ScaledRecipe};
+    use minijinja::{Environment, Value, context};
+    use test_case::test_case;
+
+    fn get_recipe_and_env<'a>(recipe: &str, template: &'a str) -> (ScaledRecipe, Environment<'a>) {
+        let recipe_parser = CooklangParser::new(Extensions::all(), Converter::default());
+        let (unscaled_recipe, _warnings) = recipe_parser.parse(recipe).into_result().unwrap();
+        let recipe = unscaled_recipe.scale(1.into(), &Converter::default());
+
+        let mut env: Environment<'a> = Environment::new();
+        env.add_template("test", template).unwrap();
+
+        (recipe, env)
+    }
+
+    #[test_case("Measure @olive oil{} into #frying pan{}.", "{{ ingredient }}", "olive oil"; "just name")]
+    #[test_case("Measure @olive oil{1} into #frying pan{}.", "{{ ingredient }}", "1 olive oil"; "name and no unit quantity")]
+    #[test_case("Measure @olive oil{1%tbsp} into #frying pan{}.", "{{ ingredient }}", "1 tbsp olive oil"; "name and quantity")]
+    #[test_case("Measure @olive oil|oil{1%tbsp} into #frying pan{}.", "{{ ingredient }}", "1 tbsp oil"; "aliased name")]
+    #[test_case("Measure @olive oil|oil{1%tbsp} into #frying pan{}.", "{{ ingredient.name }}", "olive oil"; "direct name")]
+    #[test_case("Measure @olive oil|oil{1%tbsp} into #frying pan{}.", "{{ ingredient.alias }}", "oil"; "direct alias")]
+    #[test_case("Measure @olive oil|oil{1%tbsp}(extra virgin) into #frying pan{}.", "{{ ingredient.note }}", "extra virgin"; "with note")]
+    #[test_case("Measure @olive oil|oil{1%tbsp} into #frying pan{}.", "{{ ingredient.quantity }}", "1 tbsp"; "direct quantity")]
+
+    fn ingredient(recipe: &str, template: &str, result: &str) {
+        let (recipe, env) = get_recipe_and_env(recipe, template);
+
+        // Build context
+        let context = context! {
+            ingredient => Value::from_object(Ingredient::from(recipe.ingredients[0].clone()))
+        };
+
+        let template = env.get_template("test").unwrap();
+        assert_eq!(result, template.render(context).unwrap());
+    }
+}
--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,5 +1,7 @@
 pub(crate) mod cookware;
+pub(crate) mod ingredient;
 pub(crate) mod quantity;
 
 pub(crate) use cookware::Cookware;
+pub(crate) use ingredient::Ingredient;
 pub(crate) use quantity::Quantity;
