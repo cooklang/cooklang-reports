bfb8086e3606d832194712634eaa78561695a844
Dan Fego <dan.fego@gmail.com>
2025-04-11 17:04:38 -0400

    Documented Content

--- src/model/content.rs
+++ src/model/content.rs
@@ -2,10 +2,29 @@ use super::Step;
 use serde::Serialize;
 use std::fmt::Display;
 
+/// Wrapper for [`cooklang::Content`] for reporting.
+///
+/// # Usage
+///
+/// Constructed from [`cooklang::Content`] and can be converted into [`minijinja::Value`].
+///
+/// If you have a `content`, then the following are valid ways to use it.
+///
+/// ```text
+/// {{ content }}
+/// ```
+///
+/// For the above, it is formatted according to its `Display` implementation, which renders its [`Step`][`super::Step`] or string.
 #[derive(Clone, Debug, Serialize)]
 pub enum Content {
     Step(Step),
     Text(String),
+}
+
+impl From<Content> for minijinja::Value {
+    fn from(value: Content) -> Self {
+        Self::from_object(value)
+    }
 }
 
 impl Content {
@@ -24,7 +43,7 @@ impl Display for Content {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Content::Step(step) => write!(f, "{step}"),
-            Content::Text(value) => write!(f, "{value}"),
+            Content::Text(string) => write!(f, "{string}"),
         }
     }
 }
@@ -51,10 +70,11 @@ mod tests {
     #[test_case("Rinse @potatoes{1%kg} with @water.\n\n> This recipe is great!", "{{ content }}", "1. Rinse 1 kg potatoes with water."; "interesting step")]
     fn content(recipe: &str, template: &str, expected: &str) {
         let (recipe, env) = get_recipe_and_env(recipe, template);
+        let first_content = recipe.sections[0].content[0].clone();
 
         // Build context
         let context = context! {
-            content => Value::from_object(Content::from_recipe_content(&recipe, recipe.sections[0].content[0].clone()))
+            content => Value::from(Content::from_recipe_content(&recipe, first_content))
         };
 
         let template = env.get_template("test").unwrap();
