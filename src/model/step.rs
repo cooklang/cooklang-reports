792f86dbea3ba319b1c4b27c7d8afaa8ad89ff02
Dan Fego <dan.fego@gmail.com>
2025-04-11 16:59:01 -0400

    Documented Step

--- src/model/step.rs
+++ src/model/step.rs
@@ -2,11 +2,42 @@ use super::Item;
 use serde::Serialize;
 use std::fmt::Display;
 
-// Can't do newtype anymore because we need to use my own Item.
+/// Wrapper for [`cooklang::Step`] for reporting.
+///
+/// # Usage
+///
+/// Constructed from [`cooklang::Step`] and can be converted into [`minijinja::Value`].
+///
+/// If you have a `step`, then the following are valid ways to use it.
+///
+/// ```text
+/// {{ step }}
+/// {{ step.number }}
+/// ```
+///
+/// For the above:
+///
+/// - `step` formats according to its `Display` implementation, which prints the step number and step text.
+/// - `step.number` renders the step's number.
+///
+/// The step may also be iterated over in a template, which will enumerate all its parts. This is for more
+/// advanced processing, for when the `{{ part }}` will need special rendering.
+///
+/// ```text
+/// {% for part in step %}
+/// {{ part }}
+/// {% endfor %}
+/// ```
 #[derive(Clone, Debug, Serialize)]
 pub struct Step {
     items: Vec<Item>,
     number: u32,
+}
+
+impl From<Step> for minijinja::Value {
+    fn from(value: Step) -> Self {
+        Self::from_object(value)
+    }
 }
 
 impl Step {
@@ -28,13 +59,14 @@ impl minijinja::value::Object for Step {
     }
 
     fn get_value(self: &std::sync::Arc<Self>, key: &minijinja::Value) -> Option<minijinja::Value> {
+        // If it's an index, fetch it.
+        if let Some(index) = key.as_usize() {
+            return self.items.get(index).cloned().map(minijinja::Value::from);
+        }
+
         match key.as_str()? {
             "number" => Some(minijinja::Value::from(self.number)),
-            _ => self
-                .items
-                .get(key.as_usize()?)
-                .cloned()
-                .map(minijinja::Value::from_object),
+            _ => None,
         }
     }
 
@@ -71,6 +103,9 @@ mod tests {
     #[test_case("Pour @olive oil{} into #frying pan{}.\n\nDon't burn yourself.", "{{ step }}", "1. Pour olive oil into frying pan."; "complex step")]
     #[test_case("Pour @olive oil{}\ninto #frying pan{}.\n\nDon't burn yourself.", "{{ step }}", "1. Pour olive oil into frying pan."; "multiline step")]
     #[test_case("Pour @olive oil{}\ninto #frying pan{}", "{{ step.number }}", "1"; "step number")]
+    #[test_case("Pour @olive oil{} into #frying pan{}", "{% for part in step %}{{ part }},{% endfor %}",
+        "Pour ,olive oil, into ,frying pan,"; "step parts")]
+
     fn step(recipe: &str, template: &str, expected: &str) {
         let (recipe, env) = get_recipe_and_env(recipe, template);
 
@@ -81,7 +116,7 @@ mod tests {
 
         // Build context
         let context = context! {
-            step => Value::from_object(step)
+            step => Value::from(step)
         };
 
         let template = env.get_template("test").unwrap();
